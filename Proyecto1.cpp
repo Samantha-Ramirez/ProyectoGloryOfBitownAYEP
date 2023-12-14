9786e2fa2e1d8b0e977a368f3e64827ead516e0d
Samantha-Ramirez <samantha.p.ramirez.y@gmail.com>
2023-12-14 02:00:45 -0400

    Llenar matriz

--- Proyecto1.cpp
+++ Proyecto1.cpp
@@ -2,94 +2,130 @@
 #include <stdlib.h>
 #include <math.h>
 #include <cmath>
+#include <string> //stoi
 using namespace std;
-/*
-class Tablero{
-    int filas;
-    int columnas;
-    int** tablero;
-    char casillaDefault = '.';
-    char casillaInvalida = '*';
-    string portalSalida = "PS";
-    string portalEntrada = "PE";
-    string arbol = "AR";
-    string pantano = "PA";
-    string mounstroSlime = "S";
-    string mounstroOrco = "O";
-    string mounstroGigante = "G";
-    string arquero = "AR";
-    string luchador = "LU";
 
-    Tablero(int filas, int columnas){
-        this -> filas = filas;
-		this -> columnas = columnas;
-        this -> tablero = crearTablero(filas, columnas);
-    }
-
-    int** crearTablero(int filas, int columnas){
-		int** matriz = new int*[filas];
-		for(int i = 0; i < filas; i++){
-				matriz[i] = new int[columnas];
-		}
-        return matriz;
-    }
-
-    void initTablero(){
-        for(int i = 0; i < filas; i++){
-            for(int j = 0; j < columnas; j++){
-                this -> tablero[i][j] = casillaDefault;
-            }
-        }
-    }
-
-    void printTablero(){
-        cout << endl;
-        for(int i = 0; i < filas; i++) {
-            for(int j = 0; j < columnas; j++)
-                cout << " " << tablero[i][j];
-            cout << endl;
-        }
-        cout << endl;
-    }
-
-    void borrarTablero(int** matriz, int filas, int columnas){
-		for(int i = 0; i < filas; i++){
-				delete[]matriz[i];
-		}
-		delete[]matriz;
-    }
-};
+/*TO DO
+- TIPO DE DATO MAZMORRA
+- OBTENER A, L
+- POR QUE CARACTERES RAROS EN PRIMERO 
+- RECORRIDO COMPLETO DE MAZMORRA
 */
 
+//DECLARACION PROVISIONAL DE VALORES
+char casillaDefault = '.';
+char casillaInvalida = '*';
+string portalEntrada = "PE";
+string portalSalida = "PS";
+string arbol = "AR";
+string pantano = "PA";
+string mounstroSlime = "S";
+string mounstroOrco = "O";
+string mounstroGigante = "G";
+string arquero = "AR";
+string luchador = "LU";
+
+//METODOS MAZMORRA
+string** createMazmorra(int row, int col){
+    string** maz = new string*[row];
+    for(int i = 0; i < row; i++){
+            maz[i] = new string[col];
+    }
+    /*
+    for(int i = 0; i < row; i++){
+        for(int j = 0; j < col; j++){
+            maz[i][j] = ".";
+        }
+    }
+    */
+    return maz;
+}
+
+void initMazmorra(string** mazmorra, string line, int i){
+    int x = 0;
+    for(int j = 0; j < line.size(); j++){
+        if(line[j] != ' '){
+            cout << line[j];
+            cout << mazmorra[i][x];
+            mazmorra[i][x] = line[j];
+            x++;
+        }
+    }
+}
+
+void printMazmorra(string** mazmorra, int row, int col){
+    cout << endl;
+    for(int i = 0; i < row; i++) {
+        for(int j = 0; j < col; j++)
+            cout << " " << mazmorra[i][j];
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void deleteMazmorra(string** mazmorra, int row, int col){
+    for(int i = 0; i < row; i++){
+        delete[]mazmorra[i];
+    }
+    delete[]mazmorra;
+}
+
+int cutString(string line, int &index){
+    string aux = "";
+    for(int i = index; i < line.size(); i++){
+        if(line[i] == ' ')
+            break;
+        aux += line[i];
+        index++;
+    }
+    index++;
+    return stoi(aux);
+}
+
 int main(){
-    string V = "";    //Vitalidad del Aventurero
-    string T = ""; //Tipo de Aventurero
-    string M = "";    //Cantidad de Mazmorras
-    //L A = Largo y Ancho de la Mazmorra respectivamente  
+
+    //GET DATOS DEL TXT
+    string line;
+
+    //Vitalidad del Aventurero
+    int V = -1;
+    getline(cin, line);
+    int index = 3;
+    V = cutString(line, index);
+
+    //Tipo de Aventurero
+    string T = "";
+    getline(cin, line);
+    T = line;
+
+    //Cantidad de Mazmorras 
+    int M = -1;
+    getline(cin, line);
+    M = stoi(line);
     
-    string linea;
-    string tableroString = "35$LU$1";
-    int sizeOfTableroString = 7;
-    while(getline(cin, linea)) {
-        tableroString += linea;
-        tableroString += "$";
-        //cout << linea << endl;
-    }
-    int i = 0;
-    for(; i < sizeOfTableroString; i++){
-        if(tableroString[i] == '$')
-            break;
-        V += tableroString[i];
-    }
-    for(i = i+1; i < sizeOfTableroString; i++){
-        if(tableroString[i] == '$')
-            break;
-        T += tableroString[i];
-    }
-    for(i = i+1; i < sizeOfTableroString; i++){
-        if(tableroString[i] == '$')
-            break;
-        M += tableroString[i];
+    for(int i = 0; i < M; i++){
+        //Largo de Mazmorra
+        //Ancho de Mazmorra
+        int L = -1;
+        int A = -1;
+        getline(cin, line);
+        
+        int index = 0;
+        L = cutString(line, index);
+        A = cutString(line, index);
+        
+        
+        string** mazmorra = createMazmorra(L, A);
+        printMazmorra(mazmorra, L, A);
+        for(int i = 0; i < L; i++){
+            getline(cin, line);
+            
+            initMazmorra(mazmorra, line, i);
+        }
+        
+        printMazmorra(mazmorra, L, A);
+        deleteMazmorra(mazmorra, L, A);
+        
     }
     
     return 0;
