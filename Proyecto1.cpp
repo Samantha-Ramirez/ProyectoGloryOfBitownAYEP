e53cee6bbdf3e93554c0322bf0d4ec94535a637b
Samantha-Ramirez <samantha.p.ramirez.y@gmail.com>
2023-12-14 21:39:22 -0400

    Backtracking

--- Proyecto1.cpp
+++ Proyecto1.cpp
@@ -5,15 +5,9 @@
 #include <string> //stoi
 using namespace std;
 
-/*TO DO
-- TIPO DE DATO MAZMORRA string || clase x
-- CARACTERES RAROS EN EL PRINCIPIO DEL TXT
-- RECORRIDO COMPLETO DE MAZMORRA
-*/
-
-//DECLARACION PROVISIONAL DE VALORES
-char casillaDefault = '.';
-char casillaInvalida = '*';
+//VALORES
+string casillaDefault = ".";
+string casillaInvalida = "*";
 string portalEntrada = "PE";
 string portalSalida = "PS";
 string arbol = "AR";
@@ -23,24 +17,46 @@ string mounstroOrco = "O";
 string mounstroGigante = "G";
 string arquero = "AR";
 string luchador = "LU";
+enum outputsMazmorra{
+    YOUGETTHEGLORY = 0, 
+    YOUSURVIVE = 1, 
+    YOUDIE = 2,
+};
 
-//METODOS MAZMORRA
+//BORRAR
+string tipoAventurero = luchador;
+
+//MAZMORRA
+//Movimientos
+int posibilitiesI[] = {1, 0, 0, -1};
+int posibilitiesJ[] = {0, 1, -1, 0};
+int sizePosibilities = sizeof(posibilitiesI)/sizeof(int);
+
 class Mazmorra{
     public:
-    int row;
-    int col;
-    string** mazmorra;
+    int row; //filas
+    int col; //columnas
+    int iPE; //i portal de entrada
+    int jPE; //j portal de entrada
+    int iPS; //i portal de salida
+    int jPS; //j portal de salida
+    string** mazmorra; //mazmorra
+    outputsMazmorra output; //resultado del recorrido
 
     Mazmorra(int L, int A){
         this -> row = L;
         this -> col = A;
+        this -> output = YOUDIE;
     }
+
     string** createMazmorra(){
+        //crear
         string** maz = new string*[row];
         for(int i = 0; i < row; i++){
             maz[i] = new string[col];
         }
         
+        //inicializar
         for(int i = 0; i < row; i++){
             for(int j = 0; j < col; j++){
                 maz[i][j] = "";
@@ -49,17 +65,32 @@ class Mazmorra{
         
         return maz;
     }
+
     void initMazmorra(string line, int i){
         int x = 0;
         int j = 0;
         for(int j = 0; j < line.size(); j++){
+
+            //ignorar espacio
             if(line[j] != ' '){
                 mazmorra[i][x] += line[j];
+                
+                //encontrar portal de entrada
+                if(mazmorra[i][x] == portalEntrada){
+                    this -> iPE = i;
+                    this -> jPE = x;
+                }else if(mazmorra[i][x] == portalSalida){
+                    this -> iPS = i;
+                    this -> jPS = x;
+                }
+
             }else{
+                //cambiar indice cuando hay espacio
                 x++;
             }
         }
     }
+
     void printMazmorra(){
         cout << endl;
         for(int i = 0; i < row; i++) {
@@ -69,11 +100,93 @@ class Mazmorra{
         }
         cout << endl;
     }
+
     void deleteMazmorra(){
         for(int i = 0; i < row; i++){
             delete[]mazmorra[i];
         }
         delete[]mazmorra;
+    }
+    
+    bool isMonster(string position){
+        if(position[0] == mounstroSlime[0] || position[0] == mounstroOrco[0] || position[0] == mounstroGigante[0]){
+            return true;
+        }
+        return false;
+    }
+
+    bool stillMonsters(){
+        //
+        return true;
+    }
+
+    bool isValid(int i, int j, int index){
+        i = i + posibilitiesI[index];
+        j = j + posibilitiesJ[index];
+        
+        if(i >= 0 && i < row && j >= 0 && j < col){
+            if((mazmorra[i][j] == casillaDefault)
+            || (mazmorra[i][j] == arbol && tipoAventurero == arquero)
+            || (mazmorra[i][j] == pantano && tipoAventurero == luchador)
+            || (mazmorra[i][j] == portalSalida)){
+                return true;
+
+            }else if(isMonster(mazmorra[i][j])){
+                //fight();
+            }
+        }
+        return false;
+    }
+
+    void place(int &i, int &j, int index){
+        i = i + posibilitiesI[index];
+        j = j + posibilitiesJ[index];
+    }
+
+    bool isSolution(int i, int j){
+        if(i == iPS && j == jPS){
+            return true;
+        }
+        return false;
+    }
+
+    bool isBestSolution(){
+        if(!stillMonsters()){
+            return true;
+        }
+        return false;
+    }
+    
+    void wanderMazmorra(int iIndex, int jIndex){
+        if(isSolution(iIndex, jIndex)){
+            this -> output = YOUSURVIVE;
+            if(isBestSolution()){
+                this -> output = YOUGETTHEGLORY;
+            }
+        }
+        else if(output != YOUGETTHEGLORY && iIndex * jIndex < row * col){
+            for(int i = 0; i < sizePosibilities; i++){
+                if(isValid(iIndex, jIndex, i)){
+                    //backup
+                    int auxI = iIndex;
+                    int auxJ = jIndex;
+
+                    //procesar
+                    place(iIndex, jIndex, i);
+                    string auxPosition = mazmorra[iIndex][jIndex];
+                    mazmorra[iIndex][jIndex] = casillaInvalida;       
+                    printMazmorra();
+
+                    //backtracking
+                    wanderMazmorra(iIndex, jIndex);
+
+                    //reload backup
+                    mazmorra[iIndex][jIndex] = auxPosition;
+                    iIndex = auxI;
+                    jIndex = auxJ;
+                }
+            }
+        }
     }
 };
 
@@ -95,43 +208,40 @@ int main(){
     string line;
 
     //Vitalidad del Aventurero
-    int V = -1;
     getline(cin, line);
-    int index = 3; 
-    /*Al principio del archivo salen caracteres raros, 
-    debo investigarlo pero por los momentos lo estoy cortando*/
-    V = cutStringNum(line, index);
+    int V = stoi(line);
 
     //Tipo de Aventurero
-    string T = "";
     getline(cin, line);
-    T = line;
+    string T = line;
 
     //Cantidad de Mazmorras 
-    int M = -1;
     getline(cin, line);
-    M = stoi(line);
+    int M = stoi(line);
     
     for(int i = 0; i < M; i++){
         //Largo de Mazmorra
         //Ancho de Mazmorra
-        int L = -1;
-        int A = -1;
         getline(cin, line);
         int index = 0;
-        L = cutStringNum(line, index);
-        A = cutStringNum(line, index);
+        int L = cutStringNum(line, index);
+        int A = cutStringNum(line, index);
 
-        //Mazmorra
+        //MAZMORRA
         Mazmorra mazmorra(L, A);
-        
         mazmorra.mazmorra = mazmorra.createMazmorra();
         for(int i = 0; i < L; i++){
             getline(cin, line);
             mazmorra.initMazmorra(line, i);
         }
-        
-        mazmorra.printMazmorra();
+
+        //BACKTRACKING
+        mazmorra.wanderMazmorra(mazmorra.iPE, mazmorra.jPE);
+
+        //SALIDA
+        cout << "SALIDA " << mazmorra.output << endl;
+
+        //ELIMINAR MAZMORRA
         mazmorra.deleteMazmorra();
         
     }
